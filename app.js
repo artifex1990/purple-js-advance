08195d5d76ad1c5f81ca7b0261549b0fa1c6be9c
pavel demyanov <pavel@demjanov.ru>
2025-03-06 00:30:14 +0300

    WeakSet

--- app.js
+++ app.js
@@ -1,32 +1,7 @@
 'use strict';
 
-const map = new WeakMap();
-let a = {a: 1};
-let b = {b: 1};
-map.set(a, 'testA');
-map.set(b, 'testB');
-console.log(map.get(a));
-console.log(map.has(a));
-console.log(map);
-
+let a = { a: 1 };
+let b = { b: 1 };
+const set = new WeakSet([a, b]);
 a = null;
-setTimeout(() => {
-    console.log(map);
-}, 1000);
-
-let cache = new WeakMap();
-
-function getValue(obj) {
-    if (!cache.has(obj)) {
-        const res = 1;
-        cache.set(obj, res);
-    }
-
-    return cache.get(obj);
-}
-
-
-const res = getValue(b);
-console.log(res);
-const res2 = getValue(b);
-console.log(res2);
+setTimeout(() => console.log(set), 1000);
