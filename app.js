f518ac26da2d186110708b54f3cb938ed852a5eb
pavel demyanov <pavel@demjanov.ru>
2025-03-05 16:09:18 +0300

    Sets

--- app.js
+++ app.js
@@ -1,3 +1,25 @@
 'use strict';
 
-console.log('Hello')
+const flights = ['Russia', 'USA', 'London', 'London', 'USA'];
+
+const setFlights = new Set(flights);
+
+console.log(setFlights);
+console.log(setFlights.size);
+console.log(setFlights.has('Russia'));
+setFlights.add('Paris');
+console.log(setFlights);
+setFlights.delete('USA');
+console.log(setFlights);
+
+for (const flight of setFlights) {
+    console.log(flight);
+}
+
+console.log([...setFlights]);
+
+const setObj = new Set([{a: 1}, {b: 2}, {c: 3}]);
+console.log(setObj);
+
+console.log(new Set('Hello'));
+console.log(new Set(false));
