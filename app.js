3bfab30392421d1b5b9ef8e3498919740f82c51e
pavel demyanov <pavel@demjanov.ru>
2025-03-06 00:22:52 +0300

    WeakMap

--- app.js
+++ app.js
@@ -1,10 +1,32 @@
 'use strict';
 
-let wetherMap = new Map([
-    ['Moscow', 10], 
-    ['London', 8],
-    ['Paris', 14]
-]);
+const map = new WeakMap();
+let a = {a: 1};
+let b = {b: 1};
+map.set(a, 'testA');
+map.set(b, 'testB');
+console.log(map.get(a));
+console.log(map.has(a));
+console.log(map);
 
-wetherMap = new Map([...wetherMap].map(arr => arr.reverse()));
-console.log(wetherMap);
+a = null;
+setTimeout(() => {
+    console.log(map);
+}, 1000);
+
+let cache = new WeakMap();
+
+function getValue(obj) {
+    if (!cache.has(obj)) {
+        const res = 1;
+        cache.set(obj, res);
+    }
+
+    return cache.get(obj);
+}
+
+
+const res = getValue(b);
+console.log(res);
+const res2 = getValue(b);
+console.log(res2);
