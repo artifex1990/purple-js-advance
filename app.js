06ced6f4969af984167e4ece71128b3856586772
pavel demyanov <pavel@demjanov.ru>
2025-03-05 18:18:47 +0300

    Быстрое создание Map

--- app.js
+++ app.js
@@ -1,30 +1,19 @@
 'use strict';
 
-const wetherMap = new Map();
-wetherMap
-    .set('London', '10')
-    .set('Moscow', '7');
+const wetherMap = new Map([
+    ['Moscow', 10], 
+    ['London', 8]
+]);
 
-console.log(wetherMap.get('London')); //10
-console.log(wetherMap.get('not found')); //undefined
-
-console.log(wetherMap.has('Moscow')); //true
-console.log(wetherMap.has('not found')); //false
-
-console.log(wetherMap.delete('London')); //true
-
-const arr = [1, 2, 3];
-const obj = {a:1};
-wetherMap
-    .set(1, 5)
-    .set(1, 6)
-    .set(true, 'yes')
-    .set(false, 'no')
-    .set(arr, 'array')
-    .set(obj, {b:2});
 
 console.log(wetherMap);
-console.log(wetherMap.size);
 
-console.log(wetherMap.get(arr));
-console.log(wetherMap.get(obj));
+const wetherObject = {
+    london: 10,
+    moscow: 7,
+    paris: 14
+};
+
+console.log(Object.entries(wetherObject));
+const wetherMap2 = new Map(Object.entries(wetherObject));
+console.log(wetherMap2);
