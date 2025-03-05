45b733d49b610d6e4cf70fb200f540a91a534260
pavel demyanov <pavel@demjanov.ru>
2025-03-05 19:31:16 +0300

    Упражнение - swap ключей и значений

--- app.js
+++ app.js
@@ -1,17 +1,10 @@
 'use strict';
 
-const wetherMap = new Map([
+let wetherMap = new Map([
     ['Moscow', 10], 
     ['London', 8],
     ['Paris', 14]
 ]);
 
-
-for (const [key, value] of wetherMap) {
-    console.log(key);
-    console.log(value);
-}
-
-console.log([...wetherMap]);
-console.log([...wetherMap.keys()]);
-console.log([...wetherMap.values()]);
+wetherMap = new Map([...wetherMap].map(arr => arr.reverse()));
+console.log(wetherMap);
