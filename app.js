6fa312a02c008fcdd75759d335b85817f3060e4f
pavel demyanov <pavel@demjanov.ru>
2025-03-06 21:29:21 +0300

    Упражнение - Корзина товаров

--- app.js
+++ app.js
@@ -1,23 +1,78 @@
 'use strict';
 
-const Book = function(title, author) {
-    this.title = title;
-    this.author = author;
-    this.isRead = false;
+const CartProducts = function() {
 }
 
-Book.prototype.read = function() {
-    this.isRead = true;
+CartProducts.prototype.products = [];
+
+CartProducts.prototype.addProduct = function (product) {
+    if (this.products.some(p => p.id === product.id)) {
+        console.log('Продукт с таким id уже существует!');
+        return;
+    }
+
+    if (product.name == '' || product.count == '') {
+        console.log('Поля не должны быть пустыми!');
+        return;
+    }
+
+    this.products.push(product);
+    console.log(this.cartProducts);
 }
 
-Book.prototype.cover = 'Paper';
+CartProducts.prototype.upGoods = function (id) {
+    const product = this.products.find(p => p.id === id);
+    if (!product) {
+        console.log('Продукт с таким id уже существует!');
+        return;
+    }
+    product.count++;
+    console.log('Увеличили количество товара!');
+    console.log(`Товара: ${product.count} шт.`);
+}
 
-const lordOfTheRing = new Book('Lord of the Rings', 'J. R. R. Tolkien');
-lordOfTheRing.read();
-console.log(lordOfTheRing);
-console.log(lordOfTheRing.cover);
-console.log(lordOfTheRing.hasOwnProperty('author'));
-console.log(lordOfTheRing.__proto__);
-console.log(lordOfTheRing.__proto__ === Book.prototype);
-console.log(Book.prototype.isPrototypeOf(lordOfTheRing));
-console.log(Book.prototype.isPrototypeOf(Book));
+CartProducts.prototype.upGoods = function (id) {
+    const product = this.products.find(p => p.id === id);
+    if (!product) {
+        console.log('Продукт с таким id уже существует!');
+        return;
+    }
+    product.count++;
+    console.log('Увеличили количество товара!');
+    console.log(`Товара: ${product.count} шт.`);
+}
+
+CartProducts.prototype.downGoods = function (id) {
+    const product = this.products.find(p => p.id === id);
+    if (!product) {
+        console.log('Продукт с таким id уже существует!');
+        return;
+    }
+    product.count--;
+
+    console.log('Уменьшили количество товара!');
+    if (product.count === 0) {
+        this.products.splice(this.products.indexOf(product), 1);
+        console.log('Товар удален из корзины!');
+    } else {
+        console.log(`Товара: ${product.count} шт.`);
+    }
+}
+
+
+
+
+const product = {id: 1, name: 'Молоко', count: 1};
+
+console.log(CartProducts);
+
+
+const cartProducts = new CartProducts();
+cartProducts.addProduct(product);
+cartProducts.upGoods(1);
+cartProducts.upGoods(1);
+cartProducts.downGoods(1);
+cartProducts.downGoods(1);
+cartProducts.downGoods(1);
+
+console.log(cartProducts);
