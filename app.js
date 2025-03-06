2cfec2a5ed871ffa6a3f4ff888a1924a0b785d6d
pavel demyanov <pavel@demjanov.ru>
2025-03-06 14:29:10 +0300

    Сравнение дат

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 'use strict';
 
-const data1 = new Date(2024, 10, 15);
-const data2 = new Date(2024, 11, 15);
-console.log(Number(data1));
-console.log(data2 - data1);
+const first = new Date(2024, 10, 4);
+const second = new Date(2024, 10, 4);
 
-function getDaysBetweenDates(dateFirst, dateSecond) {
-    return (dateSecond - dateFirst) / (1000 * 60 * 60 * 24);
-}
+console.log(first.getTime() < second.getTime());
+console.log(first == second);
+console.log(first === second);
 
-console.log(getDaysBetweenDates(data1, data2));
+
+console.log(first.getTime() == second.getTime());
+console.log(first.getTime() === second.getTime());
+console.log(Number(first) === Number(second));
+console.log(+first === +second);
