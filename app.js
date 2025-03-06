67a7dde057c4316e39a0a62e2a96754bfcd9f412
pavel demyanov <pavel@demjanov.ru>
2025-03-07 01:40:44 +0300

    Цепочки prototype

--- app.js
+++ app.js
@@ -1,48 +1,13 @@
 'use strict';
 
-const CartProducts = function() {
+const Book = function (title, author) {
+    this.title = title;
+    this.author = author;
 }
 
-CartProducts.prototype.products = [];
+Book.prototype.isRead = false;
 
-CartProducts.prototype.addProduct = function (product) {
-    if (this.products.some(p => p.id === product.id)) {
-        return;
-    }
-    this.products.push(product);
-}
-
-CartProducts.prototype.increaseAmount = function (id) {
-    this.products = this.products.map(product => {
-        if (product.id === id) {
-            product.count++;
-            return product;
-        }
-        return product
-    });
-}
-
-CartProducts.prototype.decreaseAmount = function (id) {
-    this.products = this.products.map(product => {
-        if (product.id === id) {
-            product.count--;
-            return product;
-        }
-        return product
-    }).filter(product => product.count > 0);
-}
-
-
-const product = {id: 1, name: 'Молоко', count: 1};
-
-console.log(CartProducts);
-debugger;
-const cartProducts = new CartProducts();
-cartProducts.addProduct(product);
-cartProducts.increaseAmount(1);
-cartProducts.increaseAmount(1);
-cartProducts.decreaseAmount(1);
-cartProducts.decreaseAmount(1);
-cartProducts.decreaseAmount(1);
-
-console.log(cartProducts);
+const lordsOfTheRings = new Book('The Lord of the Rings', 'J. R. R. Tolkien');
+console.log(lordsOfTheRings);
+console.log(lordsOfTheRings.hasOwnProperty('author'));
+console.log(Book.prototype.__proto__);
