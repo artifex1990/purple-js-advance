937c0c03648469dbc9b8bf56168a6c1239c94017
pavel demyanov <pavel@demjanov.ru>
2025-03-06 11:27:07 +0300

    Конвертация и проверка чисел

--- app.js
+++ app.js
@@ -1,7 +1,25 @@
 'use strict';
 
-let a = { a: 1 };
-let b = { b: 1 };
-const set = new WeakSet([a, b]);
-a = null;
-setTimeout(() => console.log(set), 1000);
+console.log(10 === 10.0);
+
+console.log(0.1 + 0.2 === 0.3);
+console.log(0.1 + 0.2);
+
+console.log(Number('100'));
+console.log(+'100');
+
+console.log(Number.parseInt('101 sec', 2));
+console.log(Number.parseInt('fds 101 sec', 2));
+
+console.log(Number.parseFloat('101.23 sec', 2));
+console.log(Number.parseFloat('fds 101.23 sec', 2));
+
+
+console.log(Number.isNaN(Number('324gdfgfdg')));
+console.log(Number.isNaN(10 / 0));
+console.log(Number.isFinite(10 / 0));
+console.log(Number.isFinite(10));
+console.log(Number.isFinite(Number('324gdfgfdg')));
+
+console.log(Number.isInteger(10));
+console.log(Number.isInteger(10.23));
