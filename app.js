296f0561536e37e4257cc333be18f9ee8485faa6
pavel demyanov <pavel@demjanov.ru>
2025-03-06 19:53:21 +0300

    Оператор new

--- app.js
+++ app.js
@@ -1,4 +1,13 @@
 'use strict';
 
-const a = [1];
-console.dir(a);
+const User = function(email, password) {
+    this.email = email;
+    this.password = password;
+}
+
+const user1 = new User('a@a.ru', '123456');
+console.log(user1);
+const user2 = new User('a2@a.ru', '12345');
+console.log(user2);
+
+console.log(user2 instanceof User);
