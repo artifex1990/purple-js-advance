c064b09e4d2cdd4835e1ed77ac2b09b2814dc79b
pavel demyanov <pavel@demjanov.ru>
2025-03-08 00:12:39 +0300

    Override методов

--- app.js
+++ app.js
@@ -1,59 +1,29 @@
 'use strict';
 
-/* const Book = function (title, author) {
-    this.title = title;
-    this.author = author;
-}
-
-Book.prototype.buy = function () {
-    console.log('Buy book');
-}
-
-const AudioBook = function (title, author, lenMin) {
-    // Book.call(this, title, author);
-    this.lenMin = lenMin;
-}
-
-AudioBook.prototype = Object.create(Book.prototype);
-AudioBook.prototype.constructor = AudioBook;
-AudioBook.prototype.log = function () {
-    console.log(`${this.title} - ${this.lenMin}`);
-}
-
-const book = new AudioBook('Lord of the Rings', 'Tolkien', 120);
-book.log();
-book.buy();
-console.log(book);
-
-console.log(book instanceof Book);
-console.log(book instanceof AudioBook); */
-
 class Book {
     constructor(title, author) {
         this.title = title;
         this.author = author;
     }
 
-    buy() {
-        console.log('Buy book');
+    info() {
+        console.log(`${this.title} - ${this.author}`);
     }
 }
 
-class AudioBook extends Book {
-    constructor(title, author, lenMin) {
+const book1 = new Book('The Lord of the Rings', 'J.R.R. Tolkien');
+book1.info();
+
+class EBook extends Book {
+    constructor(title, author, pages) {
         super(title, author);
-        this.lenMin = lenMin;
+        this.pages = pages;
     }
 
-    log() {
-        console.log(`${this.title} - ${this.lenMin}`);
+    info() {
+        console.log(`${this.title} - ${this.author} - ${this.pages}`);
     }
 }
 
-const book = new AudioBook('Lord of the Rings', 'Tolkien', 120);
-book.log();
-book.buy();
-console.log(book);
-
-console.log(book instanceof Book);
-console.log(book instanceof AudioBook);
+const book2 = new EBook('The Lord of the Rings', 'J.R.R. Tolkien',100);
+book2.info();
