e6c8fdc352c88089f1ab82266916dd2c2afe0d08
pavel demyanov <pavel@demjanov.ru>
2025-03-08 02:51:57 +0300

    Принцип инверсии зависимостей

--- app.js
+++ app.js
@@ -1,19 +1,31 @@
 'use strict';
 
-class Weapon {
-    cost;
-
-    dealDamage() {}
-}
-
-class Rifle extends Weapon {
-    shoot() {
-        this.dealDamage();
+class DB {
+    save(item) {
+        console.log(`Сохраняю в БД: ${item}`);
     }
 }
 
-class Sword extends Weapon {
-    strike() {
-        this.dealDamage();
+class MongoDB extends DB {
+    save(item) {
+        console.log(`Сохраняю в MongoDB: ${item}`);
     }
 }
+
+class ToDoList {
+    items = [1,2,3];
+    db;
+
+    constructor(db) {
+        this.db = db;
+    }
+
+    saveToDB() {
+        this.db.save(this.items);
+    }
+}
+
+const list1 = new ToDoList(new DB());
+list1.saveToDB();
+const list2 = new ToDoList(new MongoDB());
+list2.saveToDB();
