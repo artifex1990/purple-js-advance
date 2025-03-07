2df47b92ae40f9f6f6ec3192341d0a6dd401e5cd
pavel demyanov <pavel@demjanov.ru>
2025-03-07 10:11:15 +0300

    Setters и getters

--- app.js
+++ app.js
@@ -1,13 +1,45 @@
 'use strict';
 
-const Book = function (title, author) {
-    this.title = title;
-    this.author = author;
+const task = {
+    title: 'Task',
+    dueTo: new Date('2023/01/01'),
+
+    get isOverdue() {
+        return this.dueTo < new Date();
+    },
+
+    set isOverdue(isOverdueTask) {
+        if (!isOverdueTask) {
+            this.dueTo = new Date();
+        }
+    }
 }
 
-Book.prototype.isRead = false;
+console.log(task.isOverdue);
+task.isOverdue = false;
+console.log(task);
+console.log(task.isOverdue);
 
-const lordsOfTheRings = new Book('The Lord of the Rings', 'J. R. R. Tolkien');
-console.log(lordsOfTheRings);
-console.log(lordsOfTheRings.hasOwnProperty('author'));
-console.log(Book.prototype.__proto__);
+
+class Task {
+    constructor(title, dueDate) {
+        this.title = title;
+        this.dueDate = dueDate;
+    }
+
+    get isOverdue() {
+        return this.dueDate < new Date();
+    }
+
+    set dueDate(date) {
+        if (date < new Date()) {
+            return;
+        }
+        this._dueDate = date;
+    }
+}
+
+const newTask = new Task('Task2', new Date('2027/01/01'));
+//console.log(newTask.dueDate = new Date('2024/01/01'));
+console.log(newTask);
+// console.log(newTask.isOverdue);
