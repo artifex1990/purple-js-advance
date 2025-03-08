971c718ed46b7a24debff5154a88cf1b77d2bed9
pavel demyanov <pavel@demjanov.ru>
2025-03-09 01:10:36 +0300

    Promise withResolvers

--- app.js
+++ app.js
@@ -1,12 +1,59 @@
 'use strict';
 
-const prom = new Promise((resolve) => {
-    console.log('Constructor');
-    // for (let i = 0; i < 10000000000; i++);
-    setTimeout(() => resolve('Timer'), 1000);
-});
+
+/* Test 1 ex */
+function wait(ms) {
+	const { resolve, reject, promise } = Promise.withResolvers();
+    setTimeout(resolve, ms);
+    return promise;
+}
+
+async function run() {
+    console.log('начало');
+    await wait(1);
+    console.log('конец');
+}
+
+run();
+
+/* Test 2 ex */
+class Queue {
+    #message = [];
+    #resolve;
+    #reject;
+    #promise;
+
+    constructor() {
+        const { resolve, reject, promise } = Promise.withResolvers();
+        this.#resolve = resolve;
+        this.#reject = reject;
+        this.#promise = promise;
+    }
+
+    add(msg) {
+        this.#message.push(msg);
+        return this;
+    }
+
+    close() {
+        this.#resolve(this.#message);
+    }
+
+    error(reason) {
+        this.#reject(reason);
+    }
+
+    subscriber() {
+        return this.#promise;
+    }
+}
 
 
-prom.then((res) => console.log(res));
-Promise.reject(new Error('Error')).catch((error) => console.log(error));
-Promise.resolve('Instant').then((res) => console.log(res));
+const queue = new Queue();
+const sub1 = queue.subscriber();
+sub1.then(data => console.log(data)).catch(err => console.error(err));
+const sub2 = queue.subscriber();
+sub2.then(data => console.log(data)).catch(err => console.error(err));
+
+queue.add('msg1').add('msg2').close();
+// queue.add('msg1').add('msg2').error('Не получен последний пакет!');
