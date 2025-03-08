5954b13c60aebd0053b1f6c1f4dad429c1f2d374
pavel demyanov <pavel@demjanov.ru>
2025-03-09 01:35:09 +0300

    Упражнение - создание fetch(моё)

--- app.js
+++ app.js
@@ -1,59 +1,23 @@
 'use strict';
 
+function myFetch(url) {
+    return new Promise((resolve, reject) => {
+        const request = new XMLHttpRequest();
+        request.open('GET', url);
+        request.send();
 
-/* Test 1 ex */
-function wait(ms) {
-	const { resolve, reject, promise } = Promise.withResolvers();
-    setTimeout(resolve, ms);
-    return promise;
-}
-
-async function run() {
-    console.log('начало');
-    await wait(1);
-    console.log('конец');
-}
-
-run();
-
-/* Test 2 ex */
-class Queue {
-    #message = [];
-    #resolve;
-    #reject;
-    #promise;
-
-    constructor() {
-        const { resolve, reject, promise } = Promise.withResolvers();
-        this.#resolve = resolve;
-        this.#reject = reject;
-        this.#promise = promise;
-    }
-
-    add(msg) {
-        this.#message.push(msg);
-        return this;
-    }
-
-    close() {
-        this.#resolve(this.#message);
-    }
-
-    error(reason) {
-        this.#reject(reason);
-    }
-
-    subscriber() {
-        return this.#promise;
-    }
+        request.addEventListener('load', function() {
+            try {
+                resolve(this.response);
+            } catch (e) {
+                reject(new Error(e.message));
+            }
+        });
+    });
 }
 
 
-const queue = new Queue();
-const sub1 = queue.subscriber();
-sub1.then(data => console.log(data)).catch(err => console.error(err));
-const sub2 = queue.subscriber();
-sub2.then(data => console.log(data)).catch(err => console.error(err));
-
-queue.add('msg1').add('msg2').close();
-// queue.add('msg1').add('msg2').error('Не получен последний пакет!');
+myFetch('https://dummyjson.com/products/categories')
+    .then(resp => JSON.parse(resp))
+    .then(data => console.log(data))
+    .catch(err => console.log(err.message));
