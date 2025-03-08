359d50bf08b9f33d49c7e07d91ab451055a23564
pavel demyanov <pavel@demjanov.ru>
2025-03-08 22:24:21 +0300

    Упражнение - Функция запросов(учитель)

--- app.js
+++ app.js
@@ -1,52 +1,24 @@
 'use strict';
 
-function setSelect(arr) {
-    const filters = document.querySelector('.filter');
-    filters.innerHTML = `<select>
-        ${arr.map(el => `<option value="${el.name}">${el.name}</option>`)}
-    </select>`;
-}
-
-function getData(URL, errorMessage) {
-    return fetch(URL)
-        .then(resp => {
-            if (!resp.ok) {
-                throw new Error(errorMessage);
-            }
-            return resp.json();
-        })
-        .then(data => setSelect(data))
-        .catch(error => {
-            const el = document.querySelector('.filter');
-            el.innerHTML = error.message;
-        });
-}
-
-getData('https://dummyjson.com/products/categordies', 'Ошибка при загрузке данных');
-
-/* fetch('https://dummyjson.com/products/')
-    .then(
-        response => {
-            if (!response.ok) {
-                throw new Error('HTTP error ' + response.status);
-            }
-            return response.json();
+function getData(URL, errorMessage, method = 'GET') {
+    return fetch(URL, {
+        method
+    })
+    .then(resp => {
+        if (!resp.ok) {
+            throw new Error(`${errorMessage}: ${resp.status}`);
         }
-    )
+        return resp.json();
+    });
+}
+
+getData('https://dummyjson.com/products', 'Ошибка при загрузке данных продуктов')
     .then(({ products }) => {
-        console.log(products);
-        return fetch('https://dummyjson.com/products/'+products[5].id);
+        console.log(products)
+        return getData('https://dummyjson.com/products/' + products[0].id, 'Ошибка при получении данных о продукте');
     })
-    .then(response => {
-        if (!response.ok) {
-            throw new Error('HTTP error ' + response.status);
-        }
-        response.json();
-    })
-    .then(data => {
-        console.log(data);
-    })
-    .catch(error => {
+    .then(product => console.log(product))
+    .catch(errorMessage => {
         const el = document.querySelector('.filter');
-        el.innerHTML = error.message;
-    }); */
+        el.innerHTML = errorMessage.message;
+    });
