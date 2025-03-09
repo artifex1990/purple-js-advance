913f29f63eeb72f8ba4e8ce5b086dc369a1ead72
pavel demyanov <pavel@demjanov.ru>
2025-03-09 15:12:19 +0300

    Try catch

--- app.js
+++ app.js
@@ -1,17 +1,28 @@
 'use strict';
 
 async function getProducts(){
-    // fetch('https://dummyjson.com/products')
-    //     .then(response => response.json())
-    //     .then(({ products }) => console.log(products));
-    const productsResponse = await fetch('https://dummyjson.com/products');
-    const { products } = await productsResponse.json();
-    console.log(products);
+    try {
+        const productsResponse = await fetch('https://dummyjson.com/products');
+        if (!productsResponse.ok) {
+            throw new Error(productsResponse.status);
+        }
+        const { products } = await productsResponse.json();
+        console.log(products);
 
-    const productResponse = await fetch('https://dummyjson.com/products/' + products[0].id);
-    const product = await productResponse.json();
-    console.log(product);
+        const productResponse = await fetch('https://dummyjson.com/products/' + products[0].id);
+        const product = await productResponse.json();
+        console.log(product);
+    } catch (error) {
+        console.log(error);
+    } finally {
+        console.log('finally');
+    }
 }
-
+try {
+    const a = 5;
+    a = 10;
+} catch (error) {
+    console.log(error);
+}
 getProducts();
 console.log('end');
