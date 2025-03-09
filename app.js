5be25ac11442b98f184f51be3c9f9e3edd166d7a
pavel demyanov <pavel@demjanov.ru>
2025-03-09 22:03:59 +0300

    Визуальное положение элементов

--- app.js
+++ app.js
@@ -1,28 +1,19 @@
 'use strict';
 
-console.log(document.body);
-console.log(document.head);
+function generate(event) {
+    console.log(event.target.getBoundingClientRect());
 
-const el = document.querySelector('.wrapper');
-const el2 = document.querySelectorAll('meta');
-console.log(el);
-console.log(el2);
-const el3 = document.getElementsByClassName('wrapper');
-const el4 = document.getElementsByTagName('meta');
-console.log(el3);
-console.log(el4);
+    console.log('Page X offset ' + window.pageXOffset);
+    console.log('Page Y offset ' + window.pageYOffset);
+    console.log('Client width ' + document.documentElement.clientWidth);
+    console.log('Client height ' + document.documentElement.clientHeight);
 
-const button = document.createElement('button');
-button.innerHTML = 'Click Me';
+    const el = document.querySelector('.down');
+    const rect = el.getBoundingClientRect();
 
-const button2 = document.createElement('button');
-button2.innerHTML = 'Click Me 2';
-
-el.append(button);
-// el.prepend(button2);
-el.after(button2);
-
-function generate() {
-    console.log(el.parentNode);
-    el.remove();
+    window.scrollTo({
+        left: pageXOffset + rect.left,
+        top: pageYOffset + rect.top,
+        behavior: 'smooth'
+    });
 }
