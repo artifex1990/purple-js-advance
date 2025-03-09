132cb349e546ffe7cfa826921e35b7cebd202cf2
pavel demyanov <pavel@demjanov.ru>
2025-03-09 19:45:32 +0300

    Работа с элементами

--- app.js
+++ app.js
@@ -1,26 +1,28 @@
 'use strict';
 
-const wrapper = document.querySelector('.wrapper');
+console.log(document.body);
+console.log(document.head);
 
-async function getActivity() {
-    const response = await fetch('https://bored.api.lewagon.com/api/activity');
-    return response.json();
-}
+const el = document.querySelector('.wrapper');
+const el2 = document.querySelectorAll('meta');
+console.log(el);
+console.log(el2);
+const el3 = document.getElementsByClassName('wrapper');
+const el4 = document.getElementsByTagName('meta');
+console.log(el3);
+console.log(el4);
 
-async function generate() {
-    try {
-        wrapper.innerHTML = '';
-        const data = await Promise.all([
-            getActivity(),
-            getActivity(),
-            getActivity()
-        ]);
-        for (const el of data) {
-            const element = document.createElement('div');
-            element.innerHTML = `${el.activity}`;
-            wrapper.appendChild(element);
-        }  
-    } catch(err) {
-        console.log(err);
-    }
+const button = document.createElement('button');
+button.innerHTML = 'Click Me';
+
+const button2 = document.createElement('button');
+button2.innerHTML = 'Click Me 2';
+
+el.append(button);
+// el.prepend(button2);
+el.after(button2);
+
+function generate() {
+    console.log(el.parentNode);
+    el.remove();
 }
