80bc51331c42f521e974b87a46df08ef48e76f72
pavel demyanov <pavel@demjanov.ru>
2025-03-09 17:29:22 +0300

    Другие комбинации Promise

--- app.js
+++ app.js
@@ -1,28 +1,34 @@
 'use strict';
 
-async function getAllProducts() {
-    const response = await fetch('https://dummyjson.com/products');
-    return response.json();
-}
-
 async function getProduct(id) {
     const response = await fetch('https://dummyjson.com/products/' + id);
     return response.json();
 }
 
 async function getProductError(id) {
-    const response = await fetch('https://dummyjson.com/products123/' + id);
+    const response = await fetch('https://dummyjsonn.com/products/' + id);
     return response.json();
 }
 
 async function main() {
-    const { products } = await getAllProducts();
-    const res = Promise.all(products.map(product => getProduct(product.id)));
-    console.log(res);
-    // for (const product of products) {
-    //     const res = await getProduct(product.id);
-    //     console.log(res);
-    // }
+    const res1 = await Promise.all([
+        getProduct(1),
+        getProduct(2)
+    ]);
+    console.log(res1);
+
+    const res2 = await Promise.allSettled([
+        getProduct(1),
+        getProductError(2)
+    ]);
+    console.log(res2);
+
+    const res3 = await Promise.race([
+        getProduct(1),
+        getProductError(2)
+    ]);
+    console.log(res3);
+
 }
 
 main();
