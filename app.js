b42a7999e78024a739ab003026d51ab36211d822
pavel demyanov <pavel@demjanov.ru>
2025-03-10 10:30:37 +0300

    Перемещение по DOM

--- app.js
+++ app.js
@@ -1,15 +1,27 @@
 'use strict'
 
 const wrapper = document.querySelector('.wrapper');
+console.log(wrapper);
 
-for (let i = 0; i < 100; i++) {
-    const el = document.createElement('div');
-    el.innerHTML = `User id ${i}`;
-    el.setAttribute('data-user-id', i);
-    // el.addEventListener('click', () => console.log('Deleted user', i));
-    wrapper.append(el);
-}
-wrapper.addEventListener('click', (e) => {
-    const i = e.target.getAttribute('data-user-id');
-    console.log('Deleted user', i);
-});
+/* Вниз по дереву */
+const inner = wrapper.querySelector('.inner');
+const button = inner.querySelector('.button');
+console.log(inner);
+console.log(inner.childNodes);
+console.log(inner.children);
+
+console.log(inner.parentElement);
+console.log(inner.parentNode);
+
+/* Наверх по дереву */
+console.log(button.closest('.wrapper'));
+
+
+/* Сбоку */
+console.log(button.previousElementSibling);
+console.log(button.previousSibling);
+console.log(button.nextElementSibling);
+console.log(button.nextSibling);
+
+/* Из массива */
+console.log(button.parentElement.children[2]);
