b368f51614914f4142531d8316d5c6551a2525fc
pavel demyanov <pavel@demjanov.ru>
2025-03-10 23:44:10 +0300

    Instant

--- temp_api/index.js
+++ temp_api/index.js
@@ -1,1 +1,11 @@
 import 'temporal-polyfill/global';
+
+console.log(Temporal.Now.instant().toString());
+console.log(Temporal.Now.instant().epochSeconds);
+console.log(Temporal.Now.timeZoneId());
+console.log(Temporal.Now.zonedDateTimeISO().toString());
+console.log(Temporal.Now.zonedDateTimeISO('Europe/London').toString());
+
+const date = new Date();
+const instant = date.toTemporalInstant();
+console.log(instant.toString());
