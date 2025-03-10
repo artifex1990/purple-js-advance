fdc3d90c08fa65640d9efea654f48af1d0d49386
pavel demyanov <pavel@demjanov.ru>
2025-03-10 23:55:20 +0300

    Сравнение и изменение

--- temp_api/index.js
+++ temp_api/index.js
@@ -11,7 +11,7 @@ const instant = date.toTemporalInstant();
 console.log(instant.toString());
 
 const t1 = Temporal.Instant.from('2025-03-10T23:43:59.136+03:00');
-const t2 = Temporal.Instant.from('2025-03-10T23:43:59.136+03:00[Europe/Moscow]');
+const t2 = Temporal.Instant.from('2025-04-10T23:43:59.136+03:00[Europe/Moscow]');
 const t3 = Temporal.Instant.fromEpochSeconds(10e8);
 
 const res = new Temporal.ZonedDateTime(
@@ -25,3 +25,14 @@ console.log(Temporal.PlainDateTime.from('2024-04-18').toString());
 console.log(Temporal.PlainDateTime.from('2024-04-18').dayOfWeek);
 console.log(Temporal.PlainDateTime.from('2024-04-18').day);
 console.log(Temporal.PlainDateTime.from('2024-04-18').dayOfYear);
+
+
+console.log(res.add({
+    days: 2,
+    month: 1
+}).subtract({
+    weeks: 1
+}).toString());
+
+console.log(Temporal.Instant.compare(t1, t2));
+console.log(res.equals(res));
