12c839a923adc7256465be7659ac3bf235105644
pavel demyanov <pavel@demjanov.ru>
2025-03-12 01:14:49 +0300

    Поиск книг

--- dist/app.js
+++ dist/app.js
@@ -1153,6 +1153,11 @@
             this.state = state;
         }
 
+        search() {
+            const value = this.el.querySelector('input').value;
+            this.state.searchQuery = value;
+        }
+
         render() {
             this.el.classList.add('search');
             this.el.innerHTML = `
@@ -1170,6 +1175,12 @@
             </button>
         `;
 
+            this.el.querySelector('button').addEventListener('click', this.search.bind(this));
+            this.el.querySelector('input').addEventListener('keydown', (event) => {
+                if (event.code === 'Enter') {
+                    this.search();
+                }
+            });
             return this.el;
         }
     }
@@ -1186,16 +1197,32 @@
             super();
             this.appState = appState;
             this.appState = onChange(this.appState, this.appStateHook.bind(this));
+            this.state = onChange(this.state, this.stateHook.bind(this));
             this.setTitle('Поиск книг');
         }
 
         appStateHook(path) {
-            console.log(path);
             if (path === 'favorites') {
                 console.log(path);
             }
         }
 
+        
+        async stateHook(path) {
+            if (path === 'searchQuery') {
+                this.state.loading = true;
+                const data = await this.loadList(this.state.searchQuery, this.state.offset);
+                this.state.loading = false;
+                console.log(data);
+                this.state.list = data.docs;
+            }
+        }
+
+        async loadList(q, offset) {
+            const res = await fetch(`https://openlibrary.org/search.json?q=${q}&offset=${offset}`);
+            return res.json();
+        }
+        
         render() {
             const main = document.createElement('div');
             main.append(new Search(this.state).render());
