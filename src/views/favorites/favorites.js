463e7005806157e59de34c294c85a0c7e8b0c43b
pavel demyanov <pavel@demjanov.ru>
2025-03-12 14:52:39 +0300

    Упражнение - Избранное

---
+++ src/views/favorites/favorites.js
@@ -0,0 +1,40 @@
+import { AbstractView } from '../../common/view'
+import onChange from 'on-change';
+import { Header } from '../../components/header/header';
+import { CardList } from '../../components/card-list/card-list';
+
+export class FavoritesView extends AbstractView {
+    constructor(appState) {
+        super();
+        this.appState = appState;
+        this.appState = onChange(this.appState, this.appStateHook.bind(this));
+        this.setTitle('Мои книги');
+    }
+
+    destroy() {
+        onChange.unsubscribe(this.appState);
+        onChange.unsubscribe(this.state);
+    }
+
+    appStateHook(path) {
+        if (path === 'favorites') {
+            this.render();
+        }
+    }
+    
+    render() {
+        const favorites = document.createElement('div');
+        favorites.innerHTML = `
+            <h1>Избранные книги - ${this.appState.favorites.length}</h1>
+        `;
+        favorites.append(new CardList(this.appState, { list: this.appState.favorites }).render());
+        this.app.innerHTML = '';
+        this.app.append(favorites);
+        this.renderHeader();
+    }
+
+    renderHeader() {
+        const header = new Header(this.appState).render();
+        this.app.prepend(header);
+    }
+}
